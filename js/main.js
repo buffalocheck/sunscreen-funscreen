72923ddd090128a9df3c11440f17e39e64f50df9
Joanna Colson <joannacolson@gmail.com>
2017-06-02 11:29:16 -0700

    started weather api testing

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
+// Take a look at the openweathermaps API documentation: http://openweathermap.org/current
 
+
+// Here's what a sample url will look like:
+// 'http://api.openweathermap.org/data/2.5/weather?q=Chicago&units=imperial&appid=02e84210a52ed716535f02989864d080'
+
+// Following this format:
+// 'http://api.openweathermap.org/data/2.5/weather?q=City&units=imperial&appid=yourAppKey'
+
+console.log('JS loaded');
+
+var weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=";
+
+
+// Get an api key: http://openweathermap.org/appid#get
+var key = 'Your key here';
+
+
+$('#form1').on('submit', function(e) {
+    e.preventDefault();
+
+    var city = $('#city').val();
+
+    $.ajax({
+        url: weatherUrl + city + '&units=imperial&appid=02e84210a52ed716535f02989864d080',
+        method: 'GET',
+        success: function(response) {
+            console.log(response);
+            console.log('+++++++++++++++++');
+            console.log(response.main);
+            console.log('+++++++++++++++++');
+            console.log(response.main.temp);
+            $('#temp').html('The weather in ' + city + ' is ' + response.main.temp + ' degrees.');
+        }
+    });
+});
