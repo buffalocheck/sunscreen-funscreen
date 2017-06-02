4750d5453025c8579927798e8958f250e9aa81e2
Joanna Colson <joannacolson@gmail.com>
2017-06-02 13:04:56 -0700

    added UV Level api call

--- js/main.js
+++ js/main.js
@@ -11,9 +11,11 @@ console.log('JS loaded');
 
 var weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=";
 
+var UVLevelUrl = "http://api.openweathermap.org/v3/uvi/";
+
 
 // Get an api key: http://openweathermap.org/appid#get
-var key = 'Your key here';
+var key = '02e84210a52ed716535f02989864d080';
 
 
 $('#form1').on('submit', function(e) {
@@ -31,6 +33,28 @@ $('#form1').on('submit', function(e) {
             console.log('+++++++++++++++++');
             console.log(response.main.temp);
             $('#temp').html('The weather in ' + city + ' is ' + response.main.temp + ' degrees.');
+            // Get the UV level when the weather API call succeeds
+            var latitude = Math.round(response.coord.lat);
+            console.log(latitude);
+            var longitude = Math.round(response.coord.lon);
+            console.log(longitude);
+            // var latitude = 40.7;
+            // var longitude = -74.2;
+            var entireUrl = UVLevelUrl + latitude + ',' + longitude + '/current.json?appid=' + key;
+            console.log('Here is the entireURL:', entireUrl);
+            // Here is the ajax call for the UV Level and its results
+            $.ajax({
+                url: entireUrl,
+                method: 'GET',
+                success: function(response) {
+                    console.log(response);
+                    console.log('+++++++++++++++++');
+                    console.log(response.location);
+                    console.log('+++++++++++++++++');
+                    console.log(response.data);
+                    $('#UVLevel').html('The UV Level in ' + city + ' is ' + response.data + '.');
+                }
+            });
         }
     });
 });
