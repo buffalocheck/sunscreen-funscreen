972374ce30f1a464d75c4206b1993dc76b69856f
Joanna Colson <joannacolson@gmail.com>
2017-06-09 00:29:25 -0700

    adjusted text centering, changed UV Level to UV Index

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 // Added https://cors-anywhere.herokuapp.com/ in front of the calls to provide https to heroku
 var weatherUrl = "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q=";
 
-var UVLevelUrl = "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/v3/uvi/";
+var UVIndexUrl = "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/v3/uvi/";
 
 
 // Get an api key: http://openweathermap.org/appid#get
@@ -20,17 +20,17 @@ $('#form1').on('submit', function(e) {
         method: 'GET',
         success: function(response) {
             $('#temp').html('The weather in ' + city + ' is ' + response.main.temp + ' degrees.');
-            // Get the UV level when the weather API call succeeds
+            // Get the UV Index when the weather API call succeeds
             // lat and long must be specific decimal values or no data will return
             var latitude = Math.round(response.coord.lat);
             var longitude = Math.round(response.coord.lon);
-            var entireUrl = UVLevelUrl + latitude + ',' + longitude + '/current.json?appid=' + key;
-            // Here is the ajax call for the UV Level and its results
+            var entireUrl = UVIndexUrl + latitude + ',' + longitude + '/current.json?appid=' + key;
+            // Here is the ajax call for the UV Index and its results
             $.ajax({
                 url: entireUrl,
                 method: 'GET',
                 success: function(response) {
-                    $('#UVLevel').html('The UV Level in ' + city + ' is ' + response.data + '.');
+                    $('#UVIndex').html('The UV Index in ' + city + ' is ' + response.data + '.');
                     var spf = $('#spf').val();
                     var applyMinutes = Math.floor(spf * 32 / response.data);
                     $('#ApplyFreq').html('You should apply sunscreen every ' + applyMinutes + ' minutes.');
